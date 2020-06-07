6ebdf6f9c55aec0b8cf0b92f78ba952ec7efc87a
JonnHenry <jonnathanca9601@gmail.com>
2020-06-06 19:27:25 -0500

    Menu implementado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,37 +13,21 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
-    },
-    {
-      title: 'Outbox',
+      title: 'Curso',
       url: '/folder/Outbox',
-      icon: 'paper-plane'
+      icon: 'book-outline'
     },
     {
-      title: 'Favorites',
+      title: 'Perfil',
+      url: '/folder/Inbox',
+      icon: 'person-circle-outline'
+    },
+    {
+      title: 'Salir',
       url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      icon: 'exit-outline'
     }
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
   constructor(
     private platform: Platform,
@@ -61,9 +45,6 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
-    }
+    
   }
 }
