4eb1e6be661e646c679e10b335d9b9d18222cadd
JonnHenry <jonnathanca9601@gmail.com>
2020-06-10 00:12:56 -0500

    Integracion con el contenido del curso lista para hacer

---
+++ src/app/interfaces/IClase.interface.ts
@@ -0,0 +1,7 @@
+export default interface IClase {
+    descripcion: string;
+    calificacion: number;
+    id: number;
+    titulo: string;
+    imagenClase: string;
+}
---
+++ src/app/interfaces/IUsuarioToken.ts
@@ -0,0 +1,8 @@
+export default interface IUsuarioToken{
+    _id: string;
+    nombre:string;
+    email: string;
+    avatar: string;
+    sexo: string;
+    
+}
